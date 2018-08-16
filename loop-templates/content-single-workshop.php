026b8e8706ef27385e9b0a9e69050cce572d9ef7
Tom Woodward <twwoodward@me.com>
2018-08-16 09:30:19 -0400

    merge insanity

--- loop-templates/content-single-workshop.php
+++ loop-templates/content-single-workshop.php
@@ -8,12 +8,12 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
 	<div class="entry-content workshop-description">
 		<?php the_content(); ?>
