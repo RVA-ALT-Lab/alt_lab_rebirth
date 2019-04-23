70d74aac38eaf77715dbbe09d13dd9dd8cf87496
heyohbunkbunk <mdroberts@vcu.edu>
2019-04-23 10:21:12 -0400

    Update facet.php
    This is the removal of the aria-contol labels for the Workshop page. Oops. I'm hoping the removal of the other one is also just fine with no ill effects.

--- inc/facet.php
+++ inc/facet.php
@@ -4,7 +4,7 @@
 		<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 	</div>
 	<div class="col-md-2">
-		<a class="btn btn-workshop" data-toggle="collapse" href="#<?php echo $post->post_name?>" role="button" aria-expanded="false" aria-label="more details about the <?php the_title(); ?> workshop" aria-controls="collapseExample">+</a>		
+		<a class="btn btn-workshop" data-toggle="collapse" href="#<?php echo $post->post_name?>" role="button" aria-expanded="false" aria-label="more details about the <?php the_title(); ?> workshop">+</a>		
 	</div>
 		<div class="collapse" id="<?php echo $post->post_name?>">
 		  <div class="card card-body">
